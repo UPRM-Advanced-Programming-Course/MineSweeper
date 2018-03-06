f5e16702e88a5c087a1caa1c9f84b04ac3bf2bb6
Elvin O. Ortiz Collazo <elvin.ortiz3@upr.edu>
2018-03-06 02:18:45 -0400

    Change the size of boxes and added the recursion

--- MineSweeper/src/Main.java
+++ MineSweeper/src/Main.java
@@ -5,7 +5,7 @@ public class Main {
 		JFrame myFrame = new JFrame("Bienve's Grid");
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		myFrame.setLocation(400, 150);
-		myFrame.setSize(400, 400);
+		myFrame.setSize(900, 900);
 
 		MyPanel myPanel = new MyPanel();
 		myFrame.add(myPanel);
